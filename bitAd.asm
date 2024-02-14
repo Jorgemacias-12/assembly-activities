f16c24e4e53632b2b54504f48ab8c1d8c99beff5
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-02-14 01:26:12 -0600

    add(asm): create bitAd.asm
    Create example program that uses instructions to show the
    concept of adjustment and carriage in math operations.

---
+++ bitAd.asm
@@ -0,0 +1,13 @@
+; Program: bitAd.asm
+; Authors: Jorge Macias
+;          Diego Cervantes
+;          Christian Alvarez
+;
+; Description: An example program that uses and shows the concept of 
+;              adjustment and carriage in math operations.
+.modal small
+.stack
+.data
+.code
+  start:
+  end start
